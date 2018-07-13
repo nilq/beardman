11b44a4e1704dde1147702f828c483c7d8a98a07
niels <i.am@nilq.dk>
2018-07-14 00:01:45 +0200

    Refactor

--- game/scripts/Beard.gd
+++ game/scripts/Beard.gd
@@ -1,20 +1,13 @@
-extends KinematicBody2D
-
-var movement = preload("res://scripts/characterMovement.gd").new()
+extends Sprite
 
 onready var beard       = $Beard
 
-onready var BEARD_POS   = beard.get_position()
-onready var BEARD_SCALE = beard.get_scale()
+onready var BEARD_POS   = self.get_position()
+onready var BEARD_SCALE = self.get_scale()
 
 var is_bearding = false
 var beard_time  = 0.075
 
-
-
-func _process(delta):
-	var mouse_pos = get_viewport().get_mouse_position()
-	look_at(mouse_pos)
 
 
 func _input(event):
@@ -32,14 +25,14 @@ func fire(mouse_pos):
 	
 	
 	var beard_scale = BEARD_SCALE
-	var beard_pos   = beard.get_position()
+	var beard_pos   = self.get_position()
 	
 	var distance   = get_viewport().get_mouse_position().distance_to(self.get_global_position())
-	var dist_scale = distance / beard.get_texture().get_size().x
+	var dist_scale = distance / self.get_texture().get_size().x
 
 
 	var tween = $GunTween
-	tween.interpolate_property(beard, "scale",
+	tween.interpolate_property(self, "scale",
 	                BEARD_SCALE, Vector2(dist_scale, BEARD_SCALE.y), beard_time,
 	                Tween.TRANS_LINEAR, Tween.EASE_OUT_IN)
 					
@@ -53,8 +46,8 @@ func fire(mouse_pos):
 func retract_beard(distance):
 	var tween = $GunTween
 
-	tween.interpolate_property(beard, "scale",
-	                beard.get_scale(), BEARD_SCALE, beard_time,
+	tween.interpolate_property(self, "scale",
+	                self.get_scale(), BEARD_SCALE, beard_time,
 	                Tween.TRANS_LINEAR, Tween.EASE_OUT_IN)
 
 	tween.interpolate_callback(self, beard_time, "reload_beard")
--- game/scripts/characterMovement.gd
+++ game/scripts/characterMovement.gd
@@ -1,37 +1,29 @@
 extends KinematicBody2D
 
-var _speed = 2;
+var speed = 5
+
 var x = 0
 var y = 0
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
 
-func _ready():
-	pass
-	
 func _physics_process(delta):
-	
+	var mouse_pos = get_viewport().get_mouse_position()
+
+	look_at(mouse_pos)
+
 	x = 0
 	y = 0
-	
+
 	if Input.is_action_pressed("ui_down"):
 		y = 1
+
 	if Input.is_action_pressed("ui_up"):
 		y = -1
+
 	if Input.is_action_pressed("ui_right"):
 		x = 1
+
 	if Input.is_action_pressed("ui_left"):
 		x = -1
-	
 
-func _process(delta):
-	move(x, y);
-	pass
-
-# @param x,y -> determines the direction of movement by negative/positive integer. 0 is neutral
-func move(x,y):
-	var movement = Vector2(x*_speed, y*_speed)
-	move_and_collide(movement)
-	pass
+	move_and_collide(Vector2(x, y).normalized() * speed)
