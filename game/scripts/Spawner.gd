cef33ceb04335a6be008901adbaae99a6fb42eca
niels <i.am@nilq.dk>
2018-07-14 21:02:06 +0200

    Add restart button

--- game/scripts/Spawner.gd
+++ game/scripts/Spawner.gd
@@ -3,6 +3,8 @@ extends Node2D
 onready var world = get_node("..")
 onready var enemy_cat = preload( "res://scenes/Cat.tscn" ).instance()
 
+onready var player = world.get_node("Player")
+
 var cat_counter = 0
 var cat_range   = 100
 
@@ -14,13 +16,13 @@ func start_timer():
 
 	timer.set_wait_time(1.5)
 	timer.connect("timeout", self, "spawn_cat", [timer])
-	
+
 	self.add_child(timer)
-	
+
 	timer.start()
 
 func spawn_cat(timer):
-	if cat_counter < 50:
+	if self.get_global_position().distance_to(player.get_global_position()) < 1600:
 		var cat = enemy_cat.duplicate()
 		cat.set_position(self.get_position() + Vector2(rand_range(-cat_range, cat_range), rand_range(-cat_range, cat_range)))
 		
