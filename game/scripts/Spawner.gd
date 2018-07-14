13a51287a15040dddca740c06968d215e0269185
niels <i.am@nilq.dk>
2018-07-14 21:15:41 +0200

    Make game hard

--- game/scripts/Spawner.gd
+++ game/scripts/Spawner.gd
@@ -6,7 +6,7 @@ onready var enemy_cat = preload( "res://scenes/Cat.tscn" ).instance()
 onready var player = world.get_node("Player")
 
 var cat_counter = 0
-var cat_range   = 100
+var cat_range   = 60
 
 func _ready():
 	self.start_timer()
@@ -14,7 +14,7 @@ func _ready():
 func start_timer():
 	var timer = Timer.new()
 
-	timer.set_wait_time(1.5)
+	timer.set_wait_time(4)
 	timer.connect("timeout", self, "spawn_cat", [timer])
 
 	self.add_child(timer)
@@ -22,14 +22,15 @@ func start_timer():
 	timer.start()
 
 func spawn_cat(timer):
-	if self.get_global_position().distance_to(player.get_global_position()) < 1600:
-		var cat = enemy_cat.duplicate()
-		cat.set_position(self.get_position() + Vector2(rand_range(-cat_range, cat_range), rand_range(-cat_range, cat_range)))
+	if self.get_global_position().distance_to(player.get_global_position()) < 2600:
+		for i in range(1, 5):
+			var cat = enemy_cat.duplicate()
+			cat.set_position(self.get_position() + Vector2(rand_range(-cat_range, cat_range), rand_range(-cat_range, cat_range)))
+			
+			world.add_child(cat)
 		
-		world.add_child(cat)
-	
-		cat_counter += 1
-		
+			cat_counter += 1
+			
 		timer.stop()
-		
+
 		self.start_timer()
